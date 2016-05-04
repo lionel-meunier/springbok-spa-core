da51f0929b666b6b689fe94a28ac5e398edb01e0
Clément Tamisier <ctamisier@iocean.fr>
2016-05-04 15:41:06 +0200

    update springbok file

--- dist/springbok-spa-core.js
+++ dist/springbok-spa-core.js
@@ -12,64 +12,6 @@
 (function () {
     'use strict';
 
-    angular.module('springbok.core').service('enums', enums);
-
-    enums.$inject = ['$http', '$q', '$log', 'endpoints'];
-
-    function enums($http, $q, $log, endpoints) {
-        var isReady = false;
-
-        this.data = {};
-        this.ready = $q.defer();
-
-        /**
-         * Get all constants
-         */
-        this.load = function () {
-            var self = this;
-
-            if (isReady === false) {
-                $http.get(endpoints.get('enums')).success(function (data) {
-                    self.data = data;
-                    isReady = true;
-                    self.ready.resolve();
-                }).error(function () {
-                    $log.error('enumsService is not loaded');
-                    self.ready.reject();
-                });
-            }
-        };
-
-        /**
-         * Get ready promise
-         * @return {*}
-         */
-        this.isReady = function () {
-            return this.ready.promise;
-        };
-        /**
-         * Get enums with name in data
-         * @param enumName {String} name to enum
-         * @return {*}
-         */
-        this.getData = function (enumName) {
-            return this.data[enumName];
-        };
-
-        /**
-         * Get enums with name in data
-         * @param enumName {String} name to enum
-         * @return {*}
-         */
-        this.getDataByValue = function (enumName, valueSearch) {
-            var data = this.getData(enumName);
-            return _.findWhere(data, { value: valueSearch });
-        };
-    }
-})();
-(function () {
-    'use strict';
-
     angular.module('springbok.core').factory('httpInterceptor', httpInterceptor);
 
     httpInterceptor.$inject = ['$rootScope', '$q', 'session'];
@@ -144,6 +86,64 @@
         $translateProvider.preferredLanguage(CONFIG.app.preferredLanguage);
         $translateProvider.useMissingTranslationHandlerLog();
         $translateProvider.useSanitizeValueStrategy(null);
+    }
+})();
+(function () {
+    'use strict';
+
+    angular.module('springbok.core').service('enums', enums);
+
+    enums.$inject = ['$http', '$q', '$log', 'endpoints'];
+
+    function enums($http, $q, $log, endpoints) {
+        var isReady = false;
+
+        this.data = {};
+        this.ready = $q.defer();
+
+        /**
+         * Get all constants
+         */
+        this.load = function () {
+            var self = this;
+
+            if (isReady === false) {
+                $http.get(endpoints.get('enums')).success(function (data) {
+                    self.data = data;
+                    isReady = true;
+                    self.ready.resolve();
+                }).error(function () {
+                    $log.error('enumsService is not loaded');
+                    self.ready.reject();
+                });
+            }
+        };
+
+        /**
+         * Get ready promise
+         * @return {*}
+         */
+        this.isReady = function () {
+            return this.ready.promise;
+        };
+        /**
+         * Get enums with name in data
+         * @param enumName {String} name to enum
+         * @return {*}
+         */
+        this.getData = function (enumName) {
+            return this.data[enumName];
+        };
+
+        /**
+         * Get enums with name in data
+         * @param enumName {String} name to enum
+         * @return {*}
+         */
+        this.getDataByValue = function (enumName, valueSearch) {
+            var data = this.getData(enumName);
+            return _.findWhere(data, { value: valueSearch });
+        };
     }
 })();
 (function () {
@@ -523,6 +523,21 @@
 (function () {
     'use strict';
 
+    angular.module('springbok.core').filter('statusKey', statusKey);
+
+    function statusKey() {
+        return function (status) {
+            if (_.isNull && _.isUndefined && status === true) {
+                return 'GLOBAL_ACTIVATED';
+            } else {
+                return 'GLOBAL_DEACTIVATED';
+            }
+        };
+    }
+})();
+(function () {
+    'use strict';
+
     angular.module('springbok.core').service('encryptionUtils', encryptionUtils);
 
     function encryptionUtils() {
@@ -566,21 +581,6 @@
             });
 
             return processedUrl;
-        };
-    }
-})();
-(function () {
-    'use strict';
-
-    angular.module('springbok.core').filter('statusKey', statusKey);
-
-    function statusKey() {
-        return function (status) {
-            if (_.isNull && _.isUndefined && status === true) {
-                return 'GLOBAL_ACTIVATED';
-            } else {
-                return 'GLOBAL_DEACTIVATED';
-            }
         };
     }
 })();
