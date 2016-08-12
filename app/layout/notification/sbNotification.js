9775f49d3fe6ce579473f189951a71201544bd24
Baptiste Le Bail <blebail@iocean.fr>
2016-08-12 15:44:51 +0200

    Fix notification brute

--- app/layout/notification/sbNotification.js
+++ app/layout/notification/sbNotification.js
@@ -5,7 +5,7 @@
     
     var TEMPLATE = '<div ng-show="show" class="alert fixed-notification {{typeClass}}" style="z-index: 2000; position: fixed; width: 25%; top: 5%; right: 0.5%;">' +
                         '<p style="float: left; width: 95%;">' +
-                        '   <span ng-if="raw">message</span>' +
+                        '   <span ng-if="raw">{{message}}</span>' +
                         '   <span ng-if="!raw">{{message | translate}}</span>' +
                         '</p>' +
                         '<button type="button" class="close" ng-click="close()" width: 5%;>' +
--- dist/springbok-spa-core.js
+++ dist/springbok-spa-core.js
@@ -954,7 +954,7 @@
 
     angular.module('springbok.core').directive('sbNotification', sbNotification);
 
-    var TEMPLATE = '<div ng-show="show" class="alert fixed-notification {{typeClass}}" style="z-index: 2000; position: fixed; width: 25%; top: 5%; right: 0.5%;">' + '<p style="float: left; width: 95%;">' + '   <span ng-if="raw">message</span>' + '   <span ng-if="!raw">{{message | translate}}</span>' + '</p>' + '<button type="button" class="close" ng-click="close()" width: 5%;>' + '<i class="fa fa-times"></i>' + '</button>' + '</div>';
+    var TEMPLATE = '<div ng-show="show" class="alert fixed-notification {{typeClass}}" style="z-index: 2000; position: fixed; width: 25%; top: 5%; right: 0.5%;">' + '<p style="float: left; width: 95%;">' + '   <span ng-if="raw">{{message}}</span>' + '   <span ng-if="!raw">{{message | translate}}</span>' + '</p>' + '<button type="button" class="close" ng-click="close()" width: 5%;>' + '<i class="fa fa-times"></i>' + '</button>' + '</div>';
 
     function sbNotification() {
         return {
