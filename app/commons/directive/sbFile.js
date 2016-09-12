d1a50d9293b488ed1aa903d8c0e28b05f2160140
Clément Tamisier <ctamisier@iocean.fr>
2016-09-12 14:12:05 +0200

    add sb-file (for file required)

---
+++ app/commons/directive/sbFile.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular.module('springbok.core').directive('sbFile', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, el, attrs, ngModel) {
+                el.bind('change', function () {
+                    scope.$apply(function () {
+                        ngModel.$setViewValue(el.val());
+                        ngModel.$render();
+                    });
+                });
+            }
+        };
+    });
+
+})();
--- dist/springbok-spa-core.js
+++ dist/springbok-spa-core.js
@@ -486,6 +486,23 @@
 (function () {
     'use strict';
 
+    angular.module('springbok.core').directive('sbFile', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, el, attrs, ngModel) {
+                el.bind('change', function () {
+                    scope.$apply(function () {
+                        ngModel.$setViewValue(el.val());
+                        ngModel.$render();
+                    });
+                });
+            }
+        };
+    });
+})();
+(function () {
+    'use strict';
+
     angular.module('springbok.core').filter('statusKey', statusKey);
 
     function statusKey() {
@@ -812,30 +829,6 @@
 (function () {
     'use strict';
 
-    var TEMPLATE = '<p class="input-group"> ' + '<input type="text" class="form-control" ' + 'ng-model="$ctrl.dateModel" ' + 'ng-required="{{$ctrl.dateRequired}}" ' + 'name="{{$ctrl.dateFormFieldName}}" ' + 'placeholder="jj/mm/aaaa" ' + 'uib-datepicker-popup="dd/MM/yyyy" ' + 'is-open="$ctrl.isOpen" ' + 'on-open-focus="true" ' + 'current-text="{{\'GLOBAL_TODAY\' | translate}}" ' + 'clear-text="{{\'GLOBAL_RESET\' | translate}}" ' + 'close-text="{{\'GLOBAL_CLOSE\' | translate}}" /> ' + '<span class="input-group-btn"> ' + '<button type="button" class="btn" style="padding: 2px" ' + 'ng-click="$ctrl.open()"> ' + '<i class="ace-icon fa fa-calendar-o"></i> ' + '</button> ' + '</span> ' + '</p>' + '<div ng-messages="$ctrl.dateFormName[$ctrl.dateFormFieldName].$error"> ' + '<span ng-message="date" class="form-error-red">{{\'FORM_DATEFORMAT_INVALID\' | translate}}</span> ' + '</div>';
-
-    function sdDatePickerController() {
-        this.isOpen = false;
-
-        this.open = function () {
-            this.isOpen = true;
-        };
-    }
-
-    angular.module('springbok.core').component('sbDatePicker', {
-        template: TEMPLATE,
-        controller: sdDatePickerController,
-        bindings: {
-            dateRequired: '<',
-            dateModel: '=',
-            dateFormName: '=',
-            dateFormFieldName: '<'
-        }
-    });
-})();
-(function () {
-    'use strict';
-
     var TEMPLATE = '<ul class="breadcrumb"> ' + '<li ng-repeat="breadcrumb in $ctrl.breadcrumbs"> ' + '<a ng-if="breadcrumb.url" ' + 'ng-href="#{{breadcrumb.url}}"> ' + '{{breadcrumb.key | translate}} ' + '</a> ' + '<span ng-if="!breadcrumb.url"> ' + '{{breadcrumb.key | translate}} ' + '</span> ' + '</li> ' + '</ul>';
 
     function sbBreadcrumbsController() {
@@ -1057,4 +1050,28 @@
             }
         };
     }
+})();
+(function () {
+    'use strict';
+
+    var TEMPLATE = '<p class="input-group"> ' + '<input type="text" class="form-control" ' + 'ng-model="$ctrl.dateModel" ' + 'ng-required="{{$ctrl.dateRequired}}" ' + 'name="{{$ctrl.dateFormFieldName}}" ' + 'placeholder="jj/mm/aaaa" ' + 'uib-datepicker-popup="dd/MM/yyyy" ' + 'is-open="$ctrl.isOpen" ' + 'on-open-focus="true" ' + 'current-text="{{\'GLOBAL_TODAY\' | translate}}" ' + 'clear-text="{{\'GLOBAL_RESET\' | translate}}" ' + 'close-text="{{\'GLOBAL_CLOSE\' | translate}}" /> ' + '<span class="input-group-btn"> ' + '<button type="button" class="btn" style="padding: 2px" ' + 'ng-click="$ctrl.open()"> ' + '<i class="ace-icon fa fa-calendar-o"></i> ' + '</button> ' + '</span> ' + '</p>' + '<div ng-messages="$ctrl.dateFormName[$ctrl.dateFormFieldName].$error"> ' + '<span ng-message="date" class="form-error-red">{{\'FORM_DATEFORMAT_INVALID\' | translate}}</span> ' + '</div>';
+
+    function sdDatePickerController() {
+        this.isOpen = false;
+
+        this.open = function () {
+            this.isOpen = true;
+        };
+    }
+
+    angular.module('springbok.core').component('sbDatePicker', {
+        template: TEMPLATE,
+        controller: sdDatePickerController,
+        bindings: {
+            dateRequired: '<',
+            dateModel: '=',
+            dateFormName: '=',
+            dateFormFieldName: '<'
+        }
+    });
 })();
